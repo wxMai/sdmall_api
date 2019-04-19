a152500adffe527727e28eb89b69bdfd5b6446a2
wxmai <1299125734@qq.com>
2019-04-19 16:33:54 +0800

    update by wxmai

---
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -0,0 +1,31 @@
+package com.mmall.dao;
+
+import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+
+    int deleteByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping record);
+
+    Shipping selectByShippingIdUserId(@Param("userId")Integer userId,@Param("shippingId") Integer shippingId);
+
+    Shipping selectOneByUserId(@Param("userId")Integer userId);
+
+    List<Shipping> selectByUserId(@Param("userId")Integer userId);
+
+}
